2090e4186247792af8f44cd6b01b47babb884c36
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-11-15 15:34:38 +0530

    articleapp

--- app/src/main/java/com/ex/articleapp/SplashScreenActivity.kt
+++ app/src/main/java/com/ex/articleapp/SplashScreenActivity.kt
@@ -1,13 +1,38 @@
 package com.ex.articleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val SPLASH_TIME_OUT:Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        firebaseAuth = Firebase.auth
 
+        val firebase_user = firebaseAuth.currentUser
+
+        Handler().postDelayed({
+
+          //Checking if User is Logged or not
+            if(firebase_user != null){
+                startActivity(Intent(this,MainActivity::class.java))
+            }else{
+                startActivity(Intent(this,LogInActivity::class.java))
+            }
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
