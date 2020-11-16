e4a809ec3fae65f4a94c763d786098fd641ef5a1
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-11-16 17:46:45 +0530

    articleapp

--- app/src/main/java/com/ex/articleapp/adapter/SearchAdapter.kt
+++ app/src/main/java/com/ex/articleapp/adapter/SearchAdapter.kt
@@ -1,27 +1,38 @@
 package com.ex.articleapp.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ex.articleapp.R
 import com.ex.articleapp.data.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 
 class SearchAdapter(val context : Context , val mUserList : List<User>) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
 
+    private var TAG = "SearchAdapter"
+    var firebaseUser : FirebaseUser ?= null
+
      class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
          var circle_image_search : CircleImageView ?= null
          var search_fullname : TextView ?= null
          var search_username : TextView  ?= null
+         var button_follow : Button?= null
 
          init {
             circle_image_search =  itemView.findViewById(R.id.circle_image_search)
             search_fullname =  itemView.findViewById(R.id.fullname_text_search)
             search_username =  itemView.findViewById(R.id.username_text_search)
+            button_follow =  itemView.findViewById(R.id.button_follow_item)
          }
     }
 
@@ -37,6 +48,83 @@ class SearchAdapter(val context : Context , val mUserList : List<User>) : Recycl
         holder.search_username!!.text = current_item.username
         holder.search_fullname!!.text = current_item.fullName
 
+       checkFollowingList(current_item.uid,holder.button_follow)
+
+
+        //Follow UnFollow
+        holder.button_follow!!.setOnClickListener {
+            if(holder.button_follow!!.text.toString() == "Follow"){
+                firebaseUser = FirebaseAuth.getInstance().currentUser
+
+                val db = Firebase.firestore
+                val ref = db.collection("Follow").document(firebaseUser!!.uid)
+                val hashmap = hashMapOf(
+                        current_item.uid to true
+                )
+
+                ref.collection("Following").document(current_item.uid).set(hashmap).addOnCompleteListener {
+                    task ->
+                    if(task.isSuccessful){
+                        val db = Firebase.firestore
+                        val reff = db.collection("Follow").document(current_item.uid)
+
+                        val hashmap = hashMapOf(
+                                firebaseUser!!.uid to true
+                        )
+
+                        reff.collection("Followers").document(firebaseUser!!.uid).set(hashmap).addOnCompleteListener {
+                            task ->
+                            if(task.isSuccessful){
+                                Log.d(TAG, "onBindViewHolder: SuccessFully Followed")
+                            }
+                        }
+                    }else{
+                        Log.d(TAG, "onBindViewHolder: Something Went Wrong")
+                    }
+                }
+
+            }else{
+                firebaseUser = FirebaseAuth.getInstance().currentUser
+                val db =  Firebase.firestore
+                val ref_delete = db.collection("Follow").document(firebaseUser!!.uid).collection("Following")
+                        .document(current_item.uid).delete().addOnCompleteListener {
+                            task ->
+                            if(task.isSuccessful){
+                                val db = Firebase.firestore
+                                val ref_delete_2 = db.collection("Follow").document(current_item.uid).collection("Followers")
+                                        .document(firebaseUser!!.uid).delete().addOnCompleteListener {
+                                            task ->
+                                            Log.d(TAG, "onBindViewHolder: UnFollow SuccessFul")
+                                        }
+                            }
+                        }
+            }
+        }
+
+
+    }
+
+    private fun checkFollowingList(uid: String, buttonFollow: Button?) {
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        val db = Firebase.firestore
+        val ref = db.collection("Follow").document(firebaseUser!!.uid).collection("Following").document(uid)
+
+
+        ref.addSnapshotListener { snapshot, error ->
+
+            if (error != null) {
+                Log.w("UserAdapter", "Listen failed.", error)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d("UserAdapter", "Current data: ${snapshot.data}")
+                buttonFollow?.text = "Following"
+            } else {
+                Log.d("UserAdapter", "Current data: null")
+                buttonFollow?.text = "Follow"
+            }
+        }
     }
 
     override fun getItemCount(): Int {
