43570ad869728788fe240e66f2b7084f547919f3
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-11-16 15:53:37 +0530

    articleapp

---
+++ app/src/main/java/com/ex/articleapp/adapter/SearchAdapter.kt
@@ -0,0 +1,46 @@
+package com.ex.articleapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ex.articleapp.R
+import com.ex.articleapp.data.User
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+class SearchAdapter(val context : Context , val mUserList : List<User>) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+
+     class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+         var circle_image_search : CircleImageView ?= null
+         var search_fullname : TextView ?= null
+         var search_username : TextView  ?= null
+
+         init {
+            circle_image_search =  itemView.findViewById(R.id.circle_image_search)
+            search_fullname =  itemView.findViewById(R.id.fullname_text_search)
+            search_username =  itemView.findViewById(R.id.username_text_search)
+         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_recycler_item_list,parent,false)
+        return SearchViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        val current_item : User = mUserList[position]
+
+        Picasso.get().load(current_item.profile_photo).placeholder(R.drawable.ic_launcher_background).into(holder.circle_image_search)
+        holder.search_username!!.text = current_item.username
+        holder.search_fullname!!.text = current_item.fullName
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return mUserList.size
+    }
+}
--- app/src/main/java/com/ex/articleapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/ex/articleapp/fragments/SearchFragment.kt
@@ -1,18 +1,34 @@
 package com.ex.articleapp.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ex.articleapp.R
+import com.ex.articleapp.adapter.SearchAdapter
+import com.ex.articleapp.data.User
 import com.ex.articleapp.databinding.FragmentSearchBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class SearchFragment : Fragment() {
 
+    private val TAG = "SearchFragment"
+
     private lateinit var searchBinding: FragmentSearchBinding
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var mUser : MutableList<User> ?= null
+
+    private  var searchAdapter: SearchAdapter ?= null
 
 
     override fun onCreateView(
@@ -23,10 +39,59 @@ class SearchFragment : Fragment() {
         searchBinding = FragmentSearchBinding.inflate(inflater,container,false)
         val view = searchBinding.root
 
+        searchAdapter = context?.let { mUser?.let { it1 -> SearchAdapter(it, it1) } }
+
+        searchBinding.recyclerViewSearch.setHasFixedSize(true)
+        searchBinding.recyclerViewSearch.layoutManager = LinearLayoutManager(context)
+        searchBinding.recyclerViewSearch.adapter = searchAdapter
+
+        searchBinding.editTextSearchUser.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            if(searchBinding.editTextSearchUser.text.toString() == ""){
+                searchBinding.cancelSearch.visibility = View.GONE
+            }else{
+                searchBinding.cancelSearch.visibility = View.VISIBLE
+                retrieveUsers(s.toString().toLowerCase())
+            }
+            }
+        })
+
+        searchBinding.cancelSearch.setOnClickListener {
+            searchBinding.editTextSearchUser.text.clear()
+        }
 
 
         return view
 
+    }
+
+    private fun retrieveUsers(input: String) {
+        val db = Firebase.firestore
+
+        val ref = db.collection("Users").orderBy("fullName").startAt(input).endAt(input + "\uf8ff").get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d(TAG, "${document.id} => ${document.data}")
+                val user : User = document.toObject(User::class.java)
+                if(document != null){
+                    mUser!!.add(user)
+
+                }else{
+                    mUser!!.clear()
+                }
+            }
+            searchAdapter!!.notifyDataSetChanged()
+        }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "Error getting documents: ", exception)
+                }
     }
 
 
