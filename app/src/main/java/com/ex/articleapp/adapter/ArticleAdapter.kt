990c2cb68d7a253a1680309a58ac81c0d896c8ed
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-11-18 13:47:49 +0530

    articleapp

--- app/src/main/java/com/ex/articleapp/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/ex/articleapp/adapter/ArticleAdapter.kt
@@ -33,6 +33,7 @@ class ArticleAdapter(val context : Context,val mArticle : List<Article>) : Recyc
         var title_text : TextView ?= null
         var about_text : TextView ?= null
         var explanation_text : TextView ?= null
+        var publishername : TextView ?= null
         var comment_btn : ImageView ?= null
 
         init {
@@ -41,6 +42,7 @@ class ArticleAdapter(val context : Context,val mArticle : List<Article>) : Recyc
             title_text = itemView.findViewById(R.id.text_home_title_item_list)
             about_text = itemView.findViewById(R.id.text_home_topic_item_list)
             explanation_text = itemView.findViewById(R.id.text_home_explanation_item_list)
+            publishername = itemView.findViewById(R.id.text_home_fullname_item_list)
             comment_btn = itemView.findViewById(R.id.comment_button)
         }
     }
@@ -63,34 +65,63 @@ class ArticleAdapter(val context : Context,val mArticle : List<Article>) : Recyc
             Log.d(TAG, "onBindViewHolder: Comment Button Clicked")
         }
             
-        publishInfo(holder.circle_image_home,holder.username_text)
+        publishInfo(holder.circle_image_home,holder.username_text,holder.publishername,current_item.publisher)
         
     }
-    
+
+   
+
     override fun getItemCount(): Int {
         return mArticle.size
     }
 
-    private fun publishInfo( circleImageHome: CircleImageView?,usernameText : TextView?) {
-        firebaseAuth = Firebase.auth
-        val db = Firebase.firestore
-        val ref = db.collection("Users").document(firebaseAuth.currentUser!!.uid)
-
-        ref.addSnapshotListener {snapshot, e ->
+    private fun publishInfo(circleImageHome: CircleImageView?, usernameText: TextView?,publishername : TextView?, publisher: String) {
+            val db = Firebase.firestore
+        val ref = db.collection("Users").document(publisher)
+        
+        ref.addSnapshotListener { snapshot, e ->
             if (e != null) {
-                Log.d(TAG, "Listen failed.", e)
+                Log.w(TAG, "Listen failed.", e)
                 return@addSnapshotListener
             }
 
             if (snapshot != null && snapshot.exists()) {
                 Log.d(TAG, "Current data: ${snapshot.data}")
-                val user : User ?= snapshot.toObject(User::class.java)
+                val user : User? = snapshot.toObject(User::class.java)
                 usernameText!!.text = user!!.username
-                Picasso.get().load(user!!.profile_photo).placeholder(R.drawable.ic_launcher_background).into(circleImageHome)
+                Log.d(TAG, "publishInfo: ${user!!.username}")
+                publishername!!.text = user.fullName
+                Log.d(TAG, "publishInfo: ${user.fullName}")
+                Picasso.get().load(user.profile_photo).into(circleImageHome)
+
             } else {
                 Log.d(TAG, "Current data: null")
-            } }
-        
+            }
+        }
     }
+
+//    private fun publishInfo( circleImageHome: CircleImageView?,usernameText : TextView?) {
+//        firebaseAuth = Firebase.auth
+//        val db = Firebase.firestore
+//        val ref = db.collection("Users").document(firebaseAuth.currentUser!!.uid)
+//
+//        ref.addSnapshotListener {snapshot, e ->
+//            if (e != null) {
+//                Log.d(TAG, "Listen failed.", e)
+//                return@addSnapshotListener
+//            }
+//
+//            if (snapshot != null && snapshot.exists()) {
+//                Log.d(TAG, "Current data: ${snapshot.data}")
+//                val user : User ?= snapshot.toObject(User::class.java)
+//                usernameText!!.text = user!!.username
+//                Picasso.get().load(user!!.profile_photo).placeholder(R.drawable.ic_launcher_background).into(circleImageHome)
+//            } else {
+//                Log.d(TAG, "Current data: null")
+//            } }
+//
+//    }
+
+   
 
 }
